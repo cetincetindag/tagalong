8b9d463aad1d338ab5180f5e3b1ea8f3b86073e2
Cetin Cetindag <cetindagcetin@gmail.com>
2024-12-26 19:03:57 +0300

    syntax errors

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,25 +1,26 @@
 import { PrismaClient } from "@prisma/client";
-
 import { env } from "~/env";
 
 const isDevelopment = env.NODE_ENV === "development";
 
 const createPrismaClient = () =>
   new PrismaClient({
-    db: {
-      url: isDevelopment
-        ? process.env.DATABASE_DEV_URL
-        : process.env.DATABASE_URL,
-      log: ["query", "error", "warn"],
+    datasources: {
+      db: {
+        url: isDevelopment
+          ? process.env.DATABASE_DEV_URL
+          : process.env.DATABASE_URL,
+      },
     },
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    log: isDevelopment ? ["query", "error", "warn"] : ["error"],
   });
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
+  prisma: PrismaClient | undefined;
 };
 
 export const db = globalForPrisma.prisma ?? createPrismaClient();
 
-if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+if (env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
